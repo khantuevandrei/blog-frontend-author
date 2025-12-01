d2b168673881a4417b0d4c1cab623bd0a4d69b10
Khantuev Andrei <khantuevandrei@gmail.com>
2025-12-01 16:53:27 +0500

    Wrap App in Box component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import "./App.css";
 import { Outlet } from "react-router";
+import { Box } from "@mui/material";
+import Header from "./features/header/Header";
+import Footer from "./features/Footer/Footer";
 
 function App() {
   return (
-    <>
-      <main>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
+      <Header />
+      <Box component="main" sx={{ flexGrow: 1 }}>
         <Outlet />
-      </main>
-    </>
+      </Box>
+      <Footer />
+    </Box>
   );
 }
 
