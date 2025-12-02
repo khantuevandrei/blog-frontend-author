8fc62fc4112ee7cc7df2b6d9783f0ad7ed657224
Khantuev Andrei <khantuevandrei@gmail.com>
2025-12-02 09:22:52 +0500

    Use RooutRoutes in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import routes from "./routes/routes";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootRoutes from "./routes/RootRoutes";
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider";
-
-const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <RootRoutes />
+      </BrowserRouter>
     </AuthProvider>
   </StrictMode>
 );
