504f5d24a711123c7da026f173efbc17d571be8b
Khantuev Andrei <khantuevandrei@gmail.com>
2025-12-02 17:58:15 +0500

    Adjust styling of post list

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,72 +1,89 @@
 import {
   List,
-  ListItemButton,
+  ListItem,
   ListItemText,
-  Divider,
-  Button,
   Box,
+  Divider,
+  Typography,
+  Card,
+  CardContent,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 
 export default function PostsList({ posts }) {
   if (!posts?.length) {
-    return <p>No posts found</p>;
+    return (
+      <Box
+        sx={{
+          width: "fit-content",
+          maxWidth: 600,
+          mx: "auto",
+          display: "block",
+          textDecoration: "none",
+          mb: 2,
+          p: 2,
+          borderRadius: 2,
+          bgcolor: "background.paper",
+          boxShadow: 1,
+        }}
+      >
+        <Typography>No posts found</Typography>
+      </Box>
+    );
   }
 
   return (
     <List sx={{ width: "100%", maxWidth: 600, mx: "auto" }}>
       {posts.map((post) => (
-        <Box
+        <ListItem
           key={post.id}
-          component={Link}
-          to={`/${post.id}`}
           sx={{
             display: "block",
-            textDecoration: "none",
             mb: 2,
             p: 2,
             borderRadius: 2,
             bgcolor: "background.paper",
             boxShadow: 1,
-            transition: "0.3s",
-            "&:hover": {
-              boxShadow: 4,
-              bgcolor: "grey.100",
-            },
+            "&:hover": { boxShadow: 4, bgcolor: "grey.100" },
           }}
         >
-          <ListItemText
-            primary={post.title}
-            secondary={`Created: ${
-              post.created_at
-                ? new Date(post.created_at).toLocaleDateString()
-                : "Unknown Date"
-            } • ${
-              post.published
-                ? `Published: ${new Date(
-                    post.published_at
-                  ).toLocaleDateString()}`
-                : "Unpublished"
-            } • ${`Comments: ${post.total_comments}`}
-              `}
-            sx={{ color: "black" }}
-          />
-        </Box>
+          <Typography
+            variant="h6"
+            component={Link}
+            to={`${post.id}`}
+            sx={{ textDecoration: "none", color: "#0068bdff", fontWeight: 500 }}
+          >
+            {post.title}
+          </Typography>
+
+          <Typography
+            variant="body1"
+            color="text.primary"
+            sx={{
+              display: "-webkit-box",
+              WebkitLineClamp: 2,
+              WebkitBoxOrient: "vertical",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              mb: 1,
+            }}
+          >
+            {post.body}
+          </Typography>
+
+          <Typography variant="caption" color="text.secondary" display="block">
+            Created: {new Date(post.created_at).toLocaleDateString()}
+          </Typography>
+          <Typography variant="caption" color="text.secondary" display="block">
+            {post.published
+              ? `Published: ${new Date(post.published_at).toLocaleDateString()}`
+              : "Not published"}
+          </Typography>
+          <Typography variant="caption" color="text.secondary" display="block">
+            {post.published ? `Comments: ${post.total_comments}` : ""}
+          </Typography>
+        </ListItem>
       ))}
-      <Box sx={{ display: "flex", justifyContent: "center", mt: 6, mb: 6 }}>
-        <Button
-          component={Link}
-          to="/new"
-          variant="contained"
-          sx={{
-            bgcolor: "#1e1e1e",
-            color: "white",
-            "&:hover": { bgcolor: "#4d4d4d" },
-          }}
-        >
-          New post
-        </Button>
-      </Box>
     </List>
   );
 }
