d68f8419f3552bd7fad02d7e5e1bc6a19f666190
Khantuev Andrei <khantuevandrei@gmail.com>
2025-12-04 11:47:39 +0500

    Generalize progress bar, so it can be reused

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,25 +1,16 @@
 import { LinearProgress, Box } from "@mui/material";
 
 export default function ProgressBar({ validations }) {
-  const keys = [
-    "usernameLength",
-    "validChars",
-    "passLength",
-    "lowercase",
-    "uppercase",
-    "number",
-    "symbol",
-    "passwordsMatch",
-  ];
-
-  const values = keys.map((key) => validations[key]);
-  const strength = values.filter(Boolean).length / keys.length;
+  const values = Object.values(validations);
+  const total = values.length;
+  const passed = values.filter(Boolean).length;
+  const progress = (passed / total) * 100;
 
   return (
     <Box sx={{ mt: 1 }}>
       <LinearProgress
         variant="determinate"
-        value={strength * 100}
+        value={progress}
         sx={{ height: 10, borderRadius: 5 }}
       />
     </Box>
