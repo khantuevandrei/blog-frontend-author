073bd9caf172ef654a2bccf06f2d3b502f65ac81
Khantuev Andrei <khantuevandrei@gmail.com>
2025-12-04 17:45:47 +0500

    Modify MetaLine to show nothing when value is null

--- src/components/MetaLine.jsx
+++ src/components/MetaLine.jsx
@@ -4,13 +4,15 @@ export default function MetaLine({ label, value }) {
   return (
     <Typography variant="body2" sx={{ color: "gray" }}>
       {label}:{" "}
-      {new Date(value).toLocaleString(undefined, {
-        year: "numeric",
-        month: "short",
-        day: "numeric",
-        hour: "2-digit",
-        minute: "2-digit",
-      })}
+      {value
+        ? new Date(value).toLocaleString(undefined, {
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+          })
+        : ""}
     </Typography>
   );
 }
