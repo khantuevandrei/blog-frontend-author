848c16e5c7bb3dbc45cf4ec0840b4cd2b0470b28
Khantuev Andrei <khantuevandrei@gmail.com>
2025-12-05 13:21:09 +0500

    Wrap main in theme provider, import fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,24 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./index.css";
-import RootRoutes from "./routes/RootRoutes";
-import { BrowserRouter } from "react-router-dom";
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import { ThemeProvider } from "./context/ThemeContext";
 import { AuthProvider } from "./context/AuthProvider";
+import { BrowserRouter } from "react-router-dom";
+import RootRoutes from "./routes/RootRoutes";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
-        <RootRoutes />
-      </BrowserRouter>
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <BrowserRouter>
+          <RootRoutes />
+        </BrowserRouter>
+      </AuthProvider>
+    </ThemeProvider>
   </StrictMode>
 );
