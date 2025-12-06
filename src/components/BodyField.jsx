5559046d4219a19246f0e791e6d7f8b413014b28
Khantuev Andrei <khantuevandrei@gmail.com>
2025-12-06 14:36:58 +0500

    Create BodyField component

---
+++ src/components/BodyField.jsx
@@ -0,0 +1,17 @@
+import { TextField } from "@mui/material";
+
+export default function BodyField({ value, onChange }) {
+  return (
+    <TextField
+      label="Body"
+      name="body"
+      margin="normal"
+      value={value}
+      onChange={onChange}
+      fullWidth
+      multiline
+      rows={6}
+      required
+    />
+  );
+}
