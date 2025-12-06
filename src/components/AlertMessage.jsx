f5a59ca65a88f46bd2c6b9399b054bcb72faa7b7
Khantuev Andrei <khantuevandrei@gmail.com>
2025-12-06 09:39:08 +0500

    Add collapse to alert message. Remove unnecessary styling options

--- src/components/AlertMessage.jsx
+++ src/components/AlertMessage.jsx
@@ -1,35 +1,21 @@
-// src/components/AlertMessage.jsx
-import { Alert, useTheme } from "@mui/material";
+import { useTheme, Collapse, Alert } from "@mui/material";
 
-export default function AlertMessage({ type, children }) {
+export default function AlertMessage({ error, children }) {
   const theme = useTheme();
 
   return (
-    <Alert
-      severity={type}
-      sx={{
-        mt: 2,
-        bgcolor:
-          type === "error"
-            ? theme.palette.error.light
-            : type === "warning"
-            ? theme.palette.warning.light
-            : type === "success"
-            ? theme.palette.success.light
-            : theme.palette.info.light,
-        color:
-          type === "error"
-            ? theme.palette.error.contrastText
-            : type === "warning"
-            ? theme.palette.warning.contrastText
-            : type === "success"
-            ? theme.palette.success.contrastText
-            : theme.palette.info.contrastText,
-        fontFamily: theme.typography.fontFamily,
-        fontSize: 14,
-      }}
-    >
-      {children}
-    </Alert>
+    <Collapse in={Boolean(error)} timeout={300}>
+      <Alert
+        severity={"error"}
+        sx={{
+          mt: 2,
+          bgcolor: theme.palette.error.light,
+          color: theme.palette.error.contrastText,
+          fontSize: 14,
+        }}
+      >
+        {children}
+      </Alert>
+    </Collapse>
   );
 }
