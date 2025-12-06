0b00b31caa469d92d9bc1fab62ffa6d8e43ea6ec
Khantuev Andrei <khantuevandrei@gmail.com>
2025-12-06 09:25:07 +0500

    Move auth info to a seperate component

---
+++ src/components/AuthInfo.jsx
@@ -0,0 +1,33 @@
+import { Typography, useTheme } from "@mui/material";
+import { Link } from "react-router";
+
+export default function AuthInfo({ desc, link, nav }) {
+  const theme = useTheme();
+
+  return (
+    <Typography
+      sx={{
+        mt: 2,
+        textAlign: "center",
+        color: theme.palette.text.secondary,
+        fontSize: 14,
+      }}
+    >
+      {desc}?{" "}
+      <Typography
+        component={Link}
+        to={nav}
+        sx={{
+          color: theme.palette.primary.main,
+          textDecoration: "none",
+          fontWeight: 600,
+          "&:hover": {
+            color: theme.palette.primary.light,
+          },
+        }}
+      >
+        {link}
+      </Typography>
+    </Typography>
+  );
+}
